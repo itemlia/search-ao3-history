0ae064e50300804253992fb4fb5296d658397e84
lia harakis <lia.harakis@yahoo.com>
2025-12-02 23:17:04 +0000

    added ability to look at bottom of list

--- search ao3 history.py
+++ search ao3 history.py
@@ -1,6 +1,8 @@
 import pandas as pd
 ao3History = pd.read_excel("c:/Users/lhara/Documents/ao3_expanded1.xlsx", thousands=',')
 
+global topOrBottom
+
 ao3History["wordcount"].astype(float)
 
 totalWordcount = ao3History["wordcount"].sum()
@@ -9,18 +11,37 @@ print("total wordcount =", totalWordcount)
 print(ao3History.columns)
 print("enter column to be searched: ")
 column = input()
-print("enter top amount to be searched: ")
-topAmount = input()
-topAmount = int(topAmount)
 
-def searchHistory(column, topAmount):
 
+
+def searchHistory(column):
+
+    print("would you like to search top amounts or bottom amounts?")
+    answer0 = input()
+
+    if answer0 == "top":
+        print("enter amount to be searched: ")
+        topAmount = input()
+        topAmount = int(topAmount)
+        topOrBottom = 1
+    elif answer0 == "bottom":
+        print("enter amount to be searched: ")
+        topAmount = input()
+        topAmount = int(topAmount)
+        topOrBottom = 0
+    else:
+        print("not a valid answer - please retype: ")
+        answer0 = input()
 
     values = ao3History[column].values
     toSearch = pd.Series(values)
     result = toSearch.value_counts()
     k = topAmount
-    top = result.head(k)
+
+    if topOrBottom == 1:
+        top = result.head(k)
+    else:
+        top = result.tail(k)
 
     if k > len(result):
         print("top amount value too large please enter new one: ")
@@ -34,25 +55,9 @@ def searchHistory(column, topAmount):
     answer = input()
 
     if answer == "yes":
-        print("enter new column to be searched: ")
-        newColumn = input()
-        print("would you like to search a new top amount?")
-        answer2 = input()
-        if answer2 == "yes":
-            print("enter new top amount to be searched: ")
-            newTopAmount = input()
-            searchHistory(newColumn, newTopAmount)
-        else:
-            searchHistory(newColumn, topAmount)
+        searchHistory(column, topAmount)
     else:
-        print("would you like to search a new top amount?")
-        answer3 = input()
-        if answer3 == "yes":
-            print("enter new top amount to be searched: ")
-            newTopAmount = input()
-            searchHistory(column, newTopAmount)
-        else:
-            print("thanks for searching :)")
+        print("thanks for searching :)")
 
 
-searchHistory(column, topAmount)
+searchHistory(column)

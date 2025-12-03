126622ee3b34e26a4980ac3bde94ca4af76c5391
lia harakis <lia.harakis@yahoo.com>
2025-12-03 00:04:02 +0000

    tryna get wordcount sorting in ascending order working

--- search ao3 history.py
+++ search ao3 history.py
@@ -3,6 +3,8 @@ ao3History = pd.read_excel("c:/Users/lhara/Documents/ao3_expanded1.xlsx", thousa
 
 global topOrBottom
 
+global sort
+
 ao3History["wordcount"].astype(float)
 
 totalWordcount = ao3History["wordcount"].sum()
@@ -14,8 +16,38 @@ column = input()
 
 
 
-def searchHistory(column):
+def searchHistory(column, topAmount):
 
+    values = ao3History[column].values
+    toSearch = pd.Series(values)
+    result = toSearch.value_counts()
+    k = topAmount
+
+    if topOrBottom == 1:
+        top = result.head(k)
+    else:
+        top = result.tail(k)
+
+    if k > len(result):
+        print("amount value too large please enter new one: ")
+        topAmountRedo = input()
+        topAmountRedo = int(topAmountRedo)
+        searchHistory(column, topAmountRedo)
+    else:
+        print(top)
+
+    print("would you like to search a new column?")
+    answer = input()
+
+    if answer == "yes":
+        print("please enter a new column: ")
+        newColumn = input()
+        searchHistory(newColumn, topAmount)
+    else:
+        print("thanks for searching :)")
+
+
+def wordcountAscending():
     print("would you like to search top amounts or bottom amounts?")
     answer0 = input()
 
@@ -32,10 +64,10 @@ def searchHistory(column):
     else:
         print("not a valid answer - please retype: ")
         answer0 = input()
-
-    values = ao3History[column].values
-    toSearch = pd.Series(values)
-    result = toSearch.value_counts()
+    
+    values = ao3History["wordcount"].sort_values
+    result = pd.Series(values)
+    
     k = topAmount
 
     if topOrBottom == 1:
@@ -55,9 +87,40 @@ def searchHistory(column):
     answer = input()
 
     if answer == "yes":
-        searchHistory(column, topAmount)
+        print("please enter a new column: ")
+        newColumn = input()
+        searchHistory(newColumn, topAmount)
     else:
         print("thanks for searching :)")
 
 
-searchHistory(column)
+def checkSort():
+    print("would you like to search top amounts or bottom amounts?")
+    answer0 = input()
+
+    if answer0 == "top":
+        print("enter amount to be searched: ")
+        topAmount = input()
+        topAmount = int(topAmount)
+        topOrBottom = 1
+        searchHistory(column, topAmount)
+    elif answer0 == "bottom":
+        print("enter amount to be searched: ")
+        topAmount = input()
+        topAmount = int(topAmount)
+        topOrBottom = 0
+        searchHistory(column, topAmount)
+    else:
+        print("not a valid answer - please retype: ")
+        answer0 = input()
+
+
+if column == "wordcount":
+    print("would you like to order wordcount in ascending order?")
+    answer00 = input()
+    if answer00 == "yes":
+        wordcountAscending()
+    else:
+        checkSort()
+else:
+    checkSort()
